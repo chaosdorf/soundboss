124fac28145a3d433a5d6216bfc64327208b2474
nomaster <nomaster@chaosdorf.de>
2012-10-29 16:27:28 +0100

    Updated chaosdorf deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,13 +3,16 @@ require 'bundler/capistrano'
 set :application,     "soundboss"
 set :deploy_to,       '/srv/http/soundboss/'
 set :deploy_via,      :remote_cache
-set :main_server,     "hyperion.chaosdorf.dn42"
+set :main_server,     "webserver.chaosdorf.dn42"
 set :repository,      "https://github.com/chaosdorf/soundboss.git"
 set :scm, :git
 set :use_sudo,        false
 set :user,            "http"
-
-role :app, "hyperion.chaosdorf.dn42"
+set :default_environment, {
+  'GEM_HOME' => "$HOME/.gem",
+  'PATH' => "$PATH:$HOME/.gem/ruby/1.9.1/bin"
+}
+role :app, "webserver.chaosdorf.dn42"
 
 namespace :deploy do
   task :restart do
